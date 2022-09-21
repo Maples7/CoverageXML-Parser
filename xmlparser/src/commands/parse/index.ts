2abdde66de70a2750a3b366fa19a4f77302e42c3
Ruiyang Li <ruiyli@microsoft.com>
2022-09-21 11:07:05 +0800

    Implement read xml by lines.

--- xmlparser/src/commands/parse/index.ts
+++ xmlparser/src/commands/parse/index.ts
@@ -1,4 +1,7 @@
+import * as fs from 'node:fs'
+import * as readline from 'node:readline'
 import {Command} from '@oclif/core'
+const XmlReader = require('xml-reader')
 
 export default class parse extends Command {
 
@@ -10,5 +13,21 @@ export default class parse extends Command {
     const {args} = await this.parse(parse)
 
     this.log(`hello ${args.xmlpath}`)
+
+    const reader = XmlReader.create({stream: true});
+    const fileStream = fs.createReadStream("C:/Users/ruiyli/Downloads/test.xml")
+
+    const rl = readline.createInterface({
+      input: fileStream,
+      crlfDelay: Infinity
+    });
+
+    reader.on('tag:item', (data:any) => console.log(data));
+    reader.on('done', (data:any) => console.log(data.children.length));
+
+    for await (const line of rl) {
+      console.log(`Line from file: ${line}`);
+      reader.parse(line)
+    }
   }
 }
