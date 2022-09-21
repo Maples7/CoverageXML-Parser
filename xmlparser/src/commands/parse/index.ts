6f38a6c219206bbd87f2c80e2f841037b6733873
Ruiyang Li <ruiyli@microsoft.com>
2022-09-21 14:54:49 +0800

    Able to read source file index.

--- xmlparser/src/commands/parse/index.ts
+++ xmlparser/src/commands/parse/index.ts
@@ -6,8 +6,9 @@ const XmlReader = require('xml-reader')
 export default class parse extends Command {
 
   static description = 'Parse XML coverge file.'
-
   static args = [{name: 'xmlpath', description: 'The XML file we are going to parse.', required: true}]
+
+  private sourceFiles: Record<string, string> = {} as Record<string, string>
 
   async run(): Promise<void> {
     const {args} = await this.parse(parse)
@@ -15,18 +16,23 @@ export default class parse extends Command {
     this.log(`hello ${args.xmlpath}`)
 
     const reader = XmlReader.create({stream: true});
-    const fileStream = fs.createReadStream("C:/Users/ruiyli/Downloads/test.xml")
+    const fileStream = fs.createReadStream('C:/Users/ruiyli/Downloads/netfxTest2.xml')
 
     const rl = readline.createInterface({
       input: fileStream,
       crlfDelay: Infinity
     });
 
-    reader.on('tag:item', (data:any) => console.log(data));
-    reader.on('done', (data:any) => console.log(data.children.length));
+    reader.on('tag:SourceFileNames', (data:any) => {
+
+      const sourceFileID = data.children.find((tag:any) => tag.name === 'SourceFileID').children[0].value
+      const sourceFileName = data.children.find((tag:any) => tag.name === 'SourceFileName').children[0].value
+
+      console.log(`SourceFileID = ${sourceFileID}, SourceFileName = ${sourceFileName}`)
+      this.sourceFiles[data.SourceFileID] = data.SourceFileName
+    });
 
     for await (const line of rl) {
-      console.log(`Line from file: ${line}`);
       reader.parse(line)
     }
   }
