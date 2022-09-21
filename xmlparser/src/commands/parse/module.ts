446fc6ab0e6bdef59efb7cb51b0adfddcccdde5c
Ruiyang Li <ruiyli@microsoft.com>
2022-09-21 16:30:25 +0800

    commit to save

--- xmlparser/src/commands/parse/module.ts
+++ xmlparser/src/commands/parse/module.ts
@@ -1,11 +1,24 @@
-export class Module {
-    moduleName: string = ''
-    imageSize: string = ''
-    imageLinkTime: string = ''
+export class CoverageUnit {
+    name: string = ''
     linesCovered: string = ''
     linesPartiallyCovered: string = ''
     linesNotCovered: string = ''
     blocksCovered: string = ''
     blocksNotCovered: string = ''
+}
+
+export class Module extends CoverageUnit {
+    imageSize: string = ''
+    imageLinkTime: string = ''
     namespaceTables: string[] = []
+}
+
+export class NamespaceTable extends CoverageUnit {
+    moduleName: string = ''
+    namespaceKeyName: string = ''
+    classes: string[] = []
+}
+
+export class Class extends CoverageUnit {
+    classKeyName: string = ''
 }
